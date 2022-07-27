76245d08a62b9ea96fc448b2568fb561eec9a9c2
jimar884 <60651943+jimar884@users.noreply.github.com>
2022-07-27 18:03:55 +0900

    make simple

--- application.py
+++ application.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
-@app.route('/profile/<string:user_name>')
-def profile(user_name):
-    return '{name}さんのプロフィール画面です'.format(name=user_name)
+# @app.route('/profile/<string:user_name>')
+# def profile(user_name):
+#     return '{name}さんのプロフィール画面です'.format(name=user_name)
 
 if __name__ == '__main__':
     app.run()
